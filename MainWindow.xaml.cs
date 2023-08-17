4e7c7affc1653ec0f303a57fbf879828ef178bab
Bond_vb28 <Bond_vb28@student.itstep.org>
2023-08-17 22:44:50 +0300

    StationeryViewModel and conect

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,15 +22,39 @@ namespace Stationery_company2
     public partial class MainWindow : Window
     {
         //Data Source=MIPC\SQLEXPRESS01;Initial Catalog=Stationery_company;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
+        string connectionString = @"Data Source=MIPC\SQLEXPRESS01;Initial Catalog=Stationery_company;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        SqlConnection connection;
+        StationeryViewModel stationeryViewModel;
 
         public MainWindow()
         {
+            connection = new SqlConnection(connectionString);
+            stationeryViewModel = new StationeryViewModel();
             InitializeComponent();
         }
 
-        private void HomeLoad(object sender, RoutedEventArgs e)
+        private async void HomeLoad(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                await connection.OpenAsync();
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    MessageBox.Show("Connection is open", "Connection", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Connection is closed", "Connection", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"SqlException: {ex.Message}", "SQL Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"System Exception: {ex.Message}", "System Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
---
+++ StationeryViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stationery_company2
+{
+    class StationeryViewModel
+    {
+        public List<Stationery> Stationery { get; set; }
+
+        public StationeryViewModel()
+        {
+            Stationery = new List<Stationery>();
+        }
+
+
+        public void AddStationery(Stationery stationery)
+        {
+            Stationery.Add(stationery);
+        }
+        public void RemoveStationery(Stationery stationery)
+        {
+            Stationery.Remove(stationery);
+        }
+        public void CliarStationery(Stationery stationery)
+        {
+            Stationery.Clear();
+        }
+    }
+}
